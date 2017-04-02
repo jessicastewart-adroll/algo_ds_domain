509d76fda158cae45490b260ac5c40f18f018433
Jess Stewart <jeselistewart@gmail.com>
2017-04-02 11:33:26 -0700

    Update ds_array_m_sparse.py

--- ds_array_m_sparse.py
+++ ds_array_m_sparse.py
@@ -1,1 +1,17 @@
+from collections import defaultdict
 
+n = int(input())
+strings = [input() for i in range(n)]
+q = int(input())
+queries = [input() for i in range(q)]
+
+# build lookup
+lookup = defaultdict(lambda: 0)
+for string in strings:
+    lookup[string] += 1
+    
+# print with default
+for query in queries:
+    print(lookup.get(query, 0))
+    
+# big O = N + Q    
