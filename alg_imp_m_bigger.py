6d0ecfc9da811d40d8fb389990a6564cf289fe1d
Jess Stewart <jeselistewart@gmail.com>
2017-04-04 12:33:26 -0700

    Update alg_imp_m_bigger.py

--- alg_imp_m_bigger.py
+++ alg_imp_m_bigger.py
@@ -1,30 +1,52 @@
-def next_bigger_word(word):
-	# find smallest value greater than ones
-	# order everything after smallest to largest
+def next_lexographical_permutation(string):
+	i = len(string)-1
+	pivot = -1
 
-	end = len(word)-1
-	word_nums = [ord(c) for c in word]
-	i = 0
+	while i > 0:
+		if string[i] > string[i-1]:
+			pivot = i-1
+			break
+		i -= 1
 
-	# first case
-	while i < end:
-		if word_nums[end] > word_nums[i]:
-			word_nums[i] > word_nums[end]
-			print(''.join([chr(n) for n in word_nums]))
-			return
-		i += 1
+	if pivot < 0:
+		return 'no answer'
+		return
 
-	# second case
-	i = 0
-	smallest = -1
-	while i < end:
-		if i <
+	j = pivot+1
+	successor = None
+	while j < len(string):
+		if string[j] > string[pivot]:
+			if not successor:
+				successor = j
+			else:
+				if string[successor] >= string[j]:
+					successor = j
+		j += 1
 	
-	# third case	
-	print('no answer')
+	string[pivot], string[successor] = string[successor], string[pivot]
 
-tests = ['ab', 'bb', 'hefg', 'dhck', 'dkhc']
+	start = pivot+1
+	end = len(string)-1
+
+	while start < end:
+		string[start], string[end] = string[end], string[start]
+		start += 1
+		end -= 1
+
+	return string	
+
+
+def next_lexographical_increase(string):
+	parsed_input = [ord(c) for c in list(string)]
+	result = next_lexographical_permutation(parsed_input)
+
+	if result == 'no answer':
+		print(result)
+	else:
+		print(''.join([chr(i) for i in result]))
+
+
+test_count = int(input())
+tests = [input() for i in range(test_count)]
 for test in tests:
-	next_bigger_word(test)	
-
-# swap points -> 0, none, 2, 2, 0
+	next_lexographical_increase(test)
