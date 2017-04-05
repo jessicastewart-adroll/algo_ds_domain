58e2da898ddc9127625f65c5158cabacde2dc163
Jess Stewart <jeselistewart@gmail.com>
2017-04-04 18:12:24 -0700

    Update ds_linked_m_cycle.py

--- ds_linked_m_cycle.py
+++ ds_linked_m_cycle.py
@@ -1,33 +1,23 @@
-class Node(object):
-	def __init__(self, data = None, next_node = None):
-		self.data = data
-		self.next = next_node
+"""
+Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
 
+A Node is defined as: 
+ 
+    class Node(object):
+        def __init__(self, data = None, next_node = None):
+            self.data = data
+            self.next = next_node
+"""
 def has_cycle(head):
 	if not head:
-		print('0')
+		return False
 	else:
 		seen = set()
 		node = head
 		while node.next:
 			if node.data in seen:
-				print('1')
-				return
+				return True
 			seen.add(node.data)
 			node = node.next
 
-		print('0')
-
-head = None
-has_cycle(head) # 0
-
-head = Node(1)
-node2 = Node(2)
-node3 = Node(3)
-head.next=node2
-node2.next=node3
-node3.next=node2
-has_cycle(head) # 1
-
-node3.next=None
-has_cycle(head) # 0
+		return False
