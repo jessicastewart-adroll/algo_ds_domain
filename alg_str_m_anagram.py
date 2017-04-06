40a72872f66d1c21be5ba2df61ef48e1062b7bdf
Jess Stewart <jeselistewart@gmail.com>
2017-04-06 09:13:30 -0700

    Update alg_str_m_anagram.py

--- alg_str_m_anagram.py
+++ alg_str_m_anagram.py
@@ -7,11 +7,15 @@ def print_anagramic_pairs(string):
 			key = frozenset(Counter(string[i:j]).items())
 			counts[key] += 1
 
-	print(counts)
+	total = 0
+	for count in counts.values():
+		total += (count*(count-1))//2
+
+	print(total)	
 	
 # n = int(input())
 # strings = [input() for i in range(n)]
-#tests = ['abba', 'abcd', 'ifailuhkqq', 'hucpoltgty', 'ovarjsnrbf', 'pvmupwjjjf', 'iwwhrlkpek']
-tests = ['abba']
+# 4 0 3 2 2 6 3
+tests = ['abba', 'abcd', 'ifailuhkqq', 'hucpoltgty', 'ovarjsnrbf', 'pvmupwjjjf', 'iwwhrlkpek']
 for string in tests:
     print_anagramic_pairs(string)
