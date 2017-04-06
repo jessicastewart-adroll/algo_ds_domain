ee0e9d293273d6b425e8123c63acaf8b83fd90f9
Jess Stewart <jeselistewart@gmail.com>
2017-04-06 15:15:24 -0700

    Update alg_sort_m_full_count.py

--- alg_sort_m_full_count.py
+++ alg_sort_m_full_count.py
@@ -1,3 +1,9 @@
+'''
+[[0, 'ab'], [6, 'cd'], [0, 'ef'], [6, 'gh'], [4, 'ij'], [0, 'ab'], [6, 'cd'], [0, 'ef'], [6, 'gh'], [0, 'ij'], [4, 'that'], [3, 'be'], [0, 'to'], [1, 'be'], [5, 'question'], [1, 'or'], [2, 'not'], [4, 'is'], [2, 'to'], [4, 'the']]
+print values in order
+BUT don't print first half of ORIGINAL array
+stable sort
+'''
 n = int(input())
 inputs = [input() for i in range(n)]
 values = []
