bff175701c3222695c0881f93338e19315e6f77c
Jess Stewart <jeselistewart@gmail.com>
2017-04-06 09:03:54 -0700

    Update alg_str_m_anagram.py

--- alg_str_m_anagram.py
+++ alg_str_m_anagram.py
@@ -1,1 +1,17 @@
+from collections import Counter, defaultdict
 
+def print_anagramic_pairs(string):
+	counts = defaultdict(int)
+	for i in range(len(string)):
+		for j in range(i+1, len(string)+1):
+			key = frozenset(Counter(string[i:j]).items())
+			counts[key] += 1
+
+	print(counts)
+	
+# n = int(input())
+# strings = [input() for i in range(n)]
+#tests = ['abba', 'abcd', 'ifailuhkqq', 'hucpoltgty', 'ovarjsnrbf', 'pvmupwjjjf', 'iwwhrlkpek']
+tests = ['abba']
+for string in tests:
+    print_anagramic_pairs(string)
