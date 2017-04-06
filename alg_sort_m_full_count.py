7c5a7a945dc8281c9739d46f93a9a737444179e3
Jess Stewart <jeselistewart@gmail.com>
2017-04-06 15:12:44 -0700

    Update alg_sort_m_full_count.py

--- alg_sort_m_full_count.py
+++ alg_sort_m_full_count.py
@@ -1,1 +1,13 @@
+n = int(input())
+inputs = [input() for i in range(n)]
+values = []
 
+for i in inputs:
+    order, val = i.split(' ')
+    values.append([int(order), val])
+    
+positions = [[]]*n    
+for i, value in enumerate(values):
+    positions[value[0]].append(i)
+    
+print(positions)    
