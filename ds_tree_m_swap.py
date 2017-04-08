929dbddcfdfc21deb8957827872e8bca299dd615
Jessica Stewart <jessica.stewart@adroll.com>
2017-04-08 12:12:31 -0400

    Update ds_tree_m_swap.py

--- ds_tree_m_swap.py
+++ ds_tree_m_swap.py
@@ -1,36 +1,59 @@
-
 class TreeNode(object):
 	def __init__(self, data):
 		self.data = data
 		self.left = None
 		self.right = None
 
-def print_inorder(node):
+
+def build_tree(nodes, node, i):
+	if not node:
+		return
+
+	if (i*2)+1 < len(nodes):
+		node.left = TreeNode(nodes[(i*2)+1])
+		build_tree(nodes, node.left, (i*2)+1)
+	if (i*2)+2 < len(nodes):
+		node.right = TreeNode(nodes[(i*2)+2])
+		build_tree(nodes, node.right, (i*2)+2)
+
+
+def inorder(node):
 	if not node or node.data == -1:
 		return
 
-	print_inorder(node.left)
+	inorder(node.left)
 	print(node.data)	
-	print_inorder(node.right)
+	inorder(node.right)
 
-def build_tree(nodes, parent, depth):
-	if depth > len(nodes)-1:
-		return
 
-	parent.left = TreeNode(nodes[depth][0])	 
-	parent.right = TreeNode(nodes[depth][1])
-	build_tree(nodes, parent.left, depth+1)	
-	build_tree(nodes, parent.right, depth+2)	 
+def swap_tree(root, swap):
+	queue = [root]
+	depth = 1
+	
+	while queue:
+		node = queue.pop()
+		left = node.left
+		right = node.right
+		depth += 1
+		if depth == swap+1:
+			node.right = left
+			node.left = right
+		else:
+			queue.extend([left, right])
 
-nodes_one = [[2, 3], [-1, -1], [-1, -1]]
-swaps_one = [1, 1]
-nodes_two = [[2, 3], [4, 5], [6, -1], [-1, 7], [8, 9], [10, 11], [12, 13], [-1, 14], [-1, -1], [15, -1], [16, 17], [-1, -1], [-1, -1], [-1, -1], [-1, -1], [-1, -1], [-1, -1]]
-swaps_two = [2, 3]
-nodes_three = [[2, 3], [4, -1], [5, -1], [6, -1], [7, 8], [-1, 9], [-1, -1], [10, 11], [-1, -1], [-1, -1], [-1, -1]]
-swaps_three = [2, 4]
 
-parent = TreeNode(1)
-# build_tree(nodes_one, parent=parent, depth=0)
-# build_tree(nodes_two, parent=parent, depth=0)
-build_tree(nodes_three, parent=parent, depth=0)
-print_inorder(parent)
+def k_swaps(nodes, swaps):
+	root = TreeNode(1)
+	build_tree(nodes, root, 0)
+	for swap in swaps:
+		swap_tree(root, swap)
+		inorder(root)
+
+n = int(input())
+raw_nodes = [input().split() for i in range(n)]
+nodes = []
+for node in raw_nodes:
+    nodes.extend([int(node[0]), int(node[1])])
+t = int(input()) 
+swaps = [int(input()) for i in range(t)]
+k_swaps(nodes, swaps)
